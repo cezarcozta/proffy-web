57c018dad7514db22435edc3adbabad8f25c7d76
César Augusto Costa <cezarcozta@gmail.com>
2020-08-10 18:31:52 -0300

    adjusts

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,11 +1,68 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import warnIcon from '../../assets/images/icons/warning.svg';
+
+import './styles.css';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
+
 
 const TeacherForm: React.FC = () => {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incível que você quer dar aulas." />
+      <PageHeader title="Que incível que você quer dar aulas."
+        description="O Primeiro passo é preencher esse fomulário de inscrição."
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <Input name="name" label="Nome completo"/>
+
+          <Input name="avatar" label="Avatar"/>
+
+          <Input name="whatsapp" label="Whatsapp"/>
+
+          <Textarea name="bio" label="Biografia" />
+        </fieldset>
+
+        <fieldset>
+          <legend>Seus à aula</legend>
+
+          <Select 
+            name="subject" 
+            label="Matéria" 
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Ciências', label: 'Ciências' },
+              { value: 'Física', label: 'Física' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'História', label: 'História' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Química', label: 'Química' },
+            ]}
+          />
+
+          <Input name="cost" label="Custo da hora/aula"/>
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warnIcon} alt="Aviso importante" />
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">
+            Salvar Cadastro
+          </button>
+        </footer>
+      </main>
     </div>
   );
 };
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -4,26 +4,18 @@ import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
+import Input from '../../components/Input';
 
 const TeacherList: React.FC = () => {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Materia</label>
-            <input type="text" id="subject"/>
-          </div>
+         <Input name="subject" label="Matéria" />
 
-          <div className="input-block">
-            <label htmlFor="subject">Dia de semana</label>
-            <input type="text" id="subject"/>
-          </div>
+         <Input name="week_day" label="Dia da semana" />
 
-          <div className="input-block">
-            <label htmlFor="subject">Hora</label>
-            <input type="text" id="subject"/>
-          </div>
+         <Input type="time" name="time" label="Hora" />
         </form>
       </PageHeader>
 
